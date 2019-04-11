ab0a8660044f4fee0faaccf6288711283f8e80cb
pedrovspereira <pedrovitorino.s.p@gmail.com>
2019-04-11 00:42:37 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShireComponent } from './shire/shire.component';
+import { GondorComponent } from './gondor/gondor.component';
+import { MordorComponent } from './mordor/mordor.component';
+import { OfflineComponent } from './offline/offline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'shire', component: ShireComponent },
+  { path: 'gondor', component: GondorComponent },
+  { path: 'mordor', component: MordorComponent },
+  { path: 'offline', component: OfflineComponent },
+  { path: '', redirectTo: '/shire', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
